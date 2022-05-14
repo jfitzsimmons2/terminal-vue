1ead830b948e4529a295e3b3328d031765fd7874
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2022-05-14 14:49:22 -0400

    Update histoire

--- histoire/histoire.config.ts
+++ histoire/histoire.config.ts
@@ -1,7 +1,5 @@
-import { HstScreenshot } from '@histoire/plugin-screenshot'
 import {defineConfig} from 'histoire'
 
 export default defineConfig({ 
-  plugins: [HstScreenshot()],
-    setupFile: '/src/histoire.setup.ts'
-  })
+  setupFile: '/src/histoire.setup.ts'
+})
