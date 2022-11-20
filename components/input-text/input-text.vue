fc852773a525225bd575e186e85c8010c8954edc
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2022-11-20 13:55:48 -0500

    formatting, adding input text

--- components/input-text/input-text.vue
+++ components/input-text/input-text.vue
@@ -1,30 +1,80 @@
 <template>
-    <slot>
-        <div class="form-group">
-            <label :for="id">{{ props.label }}</label>
-            <input :id="id" v-bind="attrs" :type="props.type" :placeholder="props.placeholder" />
-        </div>
-    </slot>
+  <slot>
+    <div class="form-group">
+      <label :for="id">{{ props.label }}</label>
+      <input
+        :id="id"
+        v-bind="attrs"
+        :type="props.type"
+        :value="modelValue"
+        @input="handleInput"
+        :placeholder="props.placeholder"
+      />
+    </div>
+  </slot>
 </template>
 
 <script lang="ts">
-export type InputType = "button" | "checkbox" | "color" | "date" | "datetime-local" | "email" | "file" | "hidden" | "image" | "month" | "number" | "password" | "radio" | "range" | "reset" | "search" | "submit" | "tel" | "text" | "time" | "url" | "week";
+export type InputType =
+  | "button"
+  | "checkbox"
+  | "color"
+  | "date"
+  | "datetime-local"
+  | "email"
+  | "file"
+  | "hidden"
+  | "image"
+  | "month"
+  | "number"
+  | "password"
+  | "radio"
+  | "range"
+  | "reset"
+  | "search"
+  | "submit"
+  | "tel"
+  | "text"
+  | "time"
+  | "url"
+  | "week";
 </script>
 
 <script setup lang="ts">
-import { useAttrs } from 'vue';
-import uniqueComponentId from '../utils/unique-component-id';
+import { computed, useAttrs, watch } from "vue";
+import uniqueComponentId from "../utils/unique-component-id";
 
-const id = uniqueComponentId('tv_input_');
+const id = uniqueComponentId("tv_input_");
 const attrs = useAttrs();
 
-const props = withDefaults(defineProps<{ label: string, type: InputType, placeholder?: string }>(), {
+const emit = defineEmits(["update:modelValue"]);
+
+const handleInput = (e: Event) => {
+  changeModelValue(e.target?.value);
+};
+
+const changeModelValue = (modelValue: string) => {
+  emit("update:modelValue", modelValue);
+};
+
+const val = computed(() => {
+  return props.modelValue;
+});
+
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    type: InputType;
+    placeholder?: string;
+    modelValue: string;
+  }>(),
+  {
     placeholder: undefined,
     label: "label",
-    type: 'text'
-})
+    type: "text",
+    modelValue: "",
+  }
+);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ histoire/src/input-text.story.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import InputText from "../../components/input-text/input-text.vue";
+
+const state = ref({
+  inputText: "Input Text",
+});
+</script>
+
+<template>
+  <Story title="Input Text">
+    <InputText
+      label="Text input:"
+      type="text"
+      v-model="state.inputText"
+    ></InputText>
+    <template #controls>
+      <HstText v-model="state.inputText" title="modelValue"></HstText>
+    </template>
+  </Story>
+</template>
